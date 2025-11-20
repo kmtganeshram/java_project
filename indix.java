4afe3f3294a48e79b0a1be636becca12493c6f09
ganesh <ganeshkumawat8301@gmail.com>
2025-11-20 17:57:16 +0530

    first commit

---
+++ indix.java
@@ -0,0 +1,21 @@
+public class indix {
+    public static void main(String[] args){
+        System.out.println("Jai Shree Ram");
+        int age = 19;                      // integer number use only not decimal
+        int rollnumber = 1758732;
+        String name = "Ganesh Kumawat";    // collaction of characters (text).
+        String address = "Kuchaman-City";
+        double marks = 99.99;               // decimal numbers
+        boolean pass = true;               // either true or false
+        char grade = 'A';                  // single character use
+
+        System.out.println("Name: " + name);
+        System.out.println("Rollnumber: " + rollnumber);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Marks: " + marks);
+        System.out.println("Pass: " + pass);
+        System.out.println("Grade: " + grade);
+
+    }
+}
